dadd73550d568435f5847b5ba8cc3df0dd5e3089
Ekalb12 <blaketann03@gmail.com>
2022-05-03 16:20:47 +1000

    Added Events

---
+++ Unity Learn/Learning/Assets/Scripts/EventManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class EventManager
+{
+    public static Action<int> OnCoinsCollected; // Int == Coins Collected
+
+    
+
+}
---
+++ Unity Learn/Learning/Assets/UiManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Text CoinCounttxt;
+
+    private void OnEnable()
+    {
+        EventManager.OnCoinsCollected += OnCoinCountUpdated;
+    }
+    private void OnDisable()
+    {
+        EventManager.OnCoinsCollected -= OnCoinCountUpdated;
+    }
+
+    private void OnCoinCountUpdated(int CoinCount)
+    {
+        CoinCounttxt.text = "Coins: " + CoinCount.ToString();
+        Debug.Log("Collect xp");
+        
+    }
+    
+}
